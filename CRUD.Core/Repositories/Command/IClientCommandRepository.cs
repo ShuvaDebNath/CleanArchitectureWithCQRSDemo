ecdcf7b9b2b25f9c74d1674c4df017dc8e612a1f
ShuvaDebNath <shuvadebnathbd@gmail.com>
2022-08-30 11:23:03 +0600

    commit and remove all file

--- CRUD.Core/Repositories/Command/IClientCommandRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using CRUD.Core.Entities;
-using CRUD.Core.Repositories.Command.Base;
-
-namespace CRUD.Core.Repositories.Command
-{
-    public interface IClientCommandRepository : ICommandRepository<Client>
-    {
-    }
-}
